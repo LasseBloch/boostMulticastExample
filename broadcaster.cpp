e31998d9ebace69b70e64c2dfcbc14019722ce26
LasseBloch <lassebloch@gmail.com>
2016-03-27 11:56:23 +0200

    Cleaned up broadcaster

--- broadcaster.cpp
+++ broadcaster.cpp
@@ -9,12 +9,13 @@
 const int port = 1900;
 
 broadcaster::broadcaster(boost::asio::io_service &io_service,
-                         const boost::asio::ip::address multicast_address) : socket_(io_service), endpoint_(multicast_address, port) {
+                         const boost::asio::ip::address multicast_address) : socket_(io_service),
+                                                                             endpoint_(multicast_address, port) {
     // Create endpoint
     socket_.open(endpoint_.protocol());
 }
 
-void broadcaster::send_message(const std::string& msg) {
+void broadcaster::send_message(const std::string &msg) {
     if (socket_.is_open()) {
         socket_.async_send_to(boost::asio::buffer(msg),
                               endpoint_,
@@ -34,8 +35,8 @@ void broadcaster::send_message(const std::string& msg) {
 
 void broadcaster::send_ssdp_discover() {
     std::string msg;
-    msg +=  "M-SEARCH * HTTP/1.1\n\r";
-    msg +=  "HOST: 239.255.255.250:1900\n\r";
+    msg += "M-SEARCH * HTTP/1.1\n\r";
+    msg += "HOST: 239.255.255.250:1900\n\r";
     msg += "MAN: \"ssdp:discover\"\n\r";
     msg += "MX: 1\n\r";
     msg += "ST: ssdp:all\n\r";
@@ -43,6 +44,6 @@ void broadcaster::send_ssdp_discover() {
     send_message(msg);
 }
 
-void broadcaster::handle_send(const boost::system::error_code& error, size_t bytes_send) {
+void broadcaster::handle_send(const boost::system::error_code &error, size_t bytes_send) {
     std::cout << "Error code: " << error << " size of transmitted data: " << bytes_send << std::endl;
 }
